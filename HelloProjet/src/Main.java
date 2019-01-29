e557a842c22223efa8154742d98a884abbb4b4ce
zhuyuehua <zhuyuehua110@163.com>
2019-01-29 15:59:49 +0800

    测试2

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,12 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("增加修改");
+        System.out.println("增加修改");
+        System.out.println("增加修改");
+        System.out.println("增加修改");
+        System.out.println("增加修改");
+        System.out.println("增加修改");
+        System.out.println("增加修改");
     }
 }
