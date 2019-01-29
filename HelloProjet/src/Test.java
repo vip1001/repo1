8b22bc51722fc7dc6b86f0fbb529a2b4afcebe8b
zhuyuehua <zhuyuehua110@163.com>
2019-01-29 16:12:12 +0800

    测试4

--- HelloProjet/src/Test.java
+++ HelloProjet/src/Test.java
@@ -2,10 +2,10 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("测试分支");
-        System.out.println("测试分支");
-        System.out.println("测试分支");
-        System.out.println("测试分支");
+        System.out.println("测试分支1");
+        System.out.println("测试分支1");
+        System.out.println("测试分支1");
+        System.out.println("测试分支1");
 
 
 
